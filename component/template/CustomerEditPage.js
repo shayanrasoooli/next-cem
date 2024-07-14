7592dbe281e74e6b0b29c3c57b394fba937522bc
shayan <rasoolishayan18@gmail.com>
2024-07-14 22:54:48 +0330

    send edited data

--- component/template/CustomerEditPage.js
+++ component/template/CustomerEditPage.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Form from "../module/Form"
+import { useRouter } from 'next/router'
+import moment from 'moment';
 
 function CustomerEditPage({data , id}) {
-    console.log({data , id});
+  const date = data.date ? moment(data.date).utc().format("YYYY-MM-DD") : '';
+  const router = useRouter();
+  const [form , setForm] = useState({
+    name: data.name,
+    lastName: data.lastName,
+    email: data.email,
+    phone: data.phone || "",
+    address: data.address || "",
+    postalCode: data.postalCode || "",
+    products: data.products || "",
+    date: date|| "",
+  })
+
+  const saveHandler = async () => {
+      const res = await fetch(`http://localhost:3000/api/edit/${id}`  ,{
+        method : "PATCH" , 
+        body : JSON.stringify({ data : form }),
+        Headers : {"Content-Type" : "application/json"}
+      });
+      const data = await  res.json();
+      if (data.status === "success") {
+        router.push("/")
+      }
+}
+  const cancelHandler = () => {
+      router.push("/")
+  }
   return (
-     <div>CustomerEditPage</div>
+     <div className='customer-page'>
+      <h4>Edit Customer</h4>
+      <Form form={form} setForm={setForm} />
+      <button className='customer-page__buttons'>
+        <button className='first' onClick={cancelHandler}>cancel</button>
+        <button className='second' onClick={saveHandler}>save</button>
+      </button>
+     </div>
   )
 }
 
--- pages/edit/[customerId].js
+++ pages/edit/[customerId].js
@@ -3,25 +3,26 @@ import React, { useEffect, useState } from 'react'
 import CustomerEditPage from '../../component/template/CustomerEditPage';
 
 function Index() {
-    const [data , setData ] = useState(null);
-
+    const [data, setData] = useState(null);
     const router = useRouter();
-    const {query : {customerId} , isReady} = router;
+    const { query: { customerId } } = router;
+    console.log(customerId);
 
     useEffect(() => {
-        if (isReady) {  
-            fetch(`/api/customer/${customerId}`)
-            .then((res) => res.json())
-            .then(data => setData(data.data))
+        if (customerId) {
+            fetch(`http://localhost:3000/api/customer/${customerId}`)
+                .then((res) => res.json())
+                .then(data => setData(data.data))
+                .catch(error => console.error('Error fetching data:', error));
         }
-    } , [isReady]);
+    }, [customerId]);
 
     if (data) {
-        <CustomerEditPage data={data} id={customerId}  /> 
-    }else{
-        console.log('nothing exist');
+        return <CustomerEditPage data={data} id={customerId}  /> 
+    } else {
+        console.log('nothing exists');
+        return null; // make sure to return something if data doesn't exist
     }
-
 }
 
 export default Index
